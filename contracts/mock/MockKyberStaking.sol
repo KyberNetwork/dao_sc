58219a14c8482cb51c20cf29166f21106cc02e83
ducquangkstn <ducquang.kstn@gmail.com>
2021-02-24 14:38:11 +0700

    compress storage

--- contracts/mock/MockKyberStaking.sol
+++ contracts/mock/MockKyberStaking.sol
@@ -11,18 +11,18 @@ contract MockKyberStaking is KyberStaking {
     uint256 _startTime
   ) KyberStaking(_admin, _kncToken, _epochPeriod, _startTime) {}
 
-  function setLatestStake(address staker, uint256 amount) public {
+  function setLatestStake(address staker, uint128 amount) public {
     stakerLatestData[staker].stake = amount;
   }
 
-  function setLatestDelegatedStake(address staker, uint256 amount) public {
+  function setLatestDelegatedStake(address staker, uint128 amount) public {
     stakerLatestData[staker].delegatedStake = amount;
   }
 
   function setEpochStake(
     address staker,
     uint256 epoch,
-    uint256 amount
+    uint128 amount
   ) public {
     stakerPerEpochData[epoch][staker].stake = amount;
   }
@@ -30,13 +30,13 @@ contract MockKyberStaking is KyberStaking {
   function setEpochDelegatedStake(
     address staker,
     uint256 epoch,
-    uint256 amount
+    uint128 amount
   ) public {
     stakerPerEpochData[epoch][staker].delegatedStake = amount;
   }
 
   function getHasInitedValue(address staker, uint256 epoch) public view returns (bool) {
-    return hasInited[epoch][staker];
+    return stakerPerEpochData[epoch][staker].hasInited;
   }
 
   function getStakesValue(address staker, uint256 epoch) public view returns (uint256) {
