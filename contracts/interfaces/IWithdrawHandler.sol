28d6d77ec343168d0bbbf2f86e688f3e37f2dc68
ducquangkstn <ducquang.kstn@gmail.com>
2021-03-08 17:00:04 +0700

    voting power strategy restructure

--- contracts/interfaces/IWithdrawHandler.sol
+++ contracts/interfaces/IWithdrawHandler.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.7.6;
 pragma abicoder v2;
 
+/**
+ * @title Interface for callbacks hooks when user withdraw from staking contract
+ */
 interface IWithdrawHandler {
   function handleWithdrawal(address staker, uint256 reduceAmount) external;
 }
